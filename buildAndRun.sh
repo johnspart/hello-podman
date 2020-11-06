fed890095b08fdcb2961ccec856da54ff0330596
John López <john.jaloar@gmail.com>
2020-11-06 14:20:49 -0500

    add description

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-
+#Stop and delete container
 podman stop example_container && podman rm -f example_container
-
+#Delete container image
 podman rmi example_image
-
+#Build Pod, 
 podman build -t example_image -f ./Podmanfile 
-
-podman run -itd -p 8080:80 --name example_container example_image
+#Stop and delete Pod
+podman pod stop example_pod && podman pod rm example_pod
+#Create new pod and run container
+podman run -dt --pod new:example_pod -p 8080:80 --name example_container example_image
